5670cdc17860e3e6f7827f10311b7f37c038c0cf
Mirza Sahin <salihmirza123@gmail.com>
2024-07-08 07:51:19 +0300

    Refactor DataTable component for readability and modularity

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -5,7 +5,7 @@ const fetcher = (url: string) => fetch(url).then(res => res.json());
 
 const useFetchData = () => {
   // CoinGecko API'den veri çekme
-  const { data, error } = useSWR('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true', fetcher, {
+  const { data, error } = useSWR('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&page=1&sparkline=true', fetcher, {
     refreshInterval: 45000, // Her 45 saniyede bir veriyi güncelle
     revalidateOnFocus: false, // Sayfa odağa geldiğinde tekrar validasyon yapma
     dedupingInterval: 45000, // Aynı URL için 45 saniye boyunca tekrar istek gönderme
@@ -18,7 +18,7 @@ const useFetchData = () => {
 
   // Veriyi ve loading durumunu döndürme
   return {
-    data: data ? data.slice(0, 10) : [], // İlk 10 kripto para birimini al
+    data: data ? data.slice(0, 15) : [], // İlk 15 kripto para birimini al
     loading: !data && !error, // Veri yoksa loading
   };
 };
---
+++ src/utils/formatters.ts
@@ -0,0 +1,27 @@
+export const formatNumber = (num: number): string => {
+    if (num >= 1_000_000_000_000) {
+      const formatted = (num / 1_000_000_000_000).toFixed(2);
+      return `${formatted.length > 7 ? (num / 1_000_000_000_000).toFixed(1) + "T" : formatted + "T"}`;
+    }
+    if (num >= 1_000_000_000) {
+      const formatted = (num / 1_000_000_000).toFixed(2);
+      return `${formatted.length > 7 ? (num / 1_000_000_000).toFixed(1) + "B" : formatted + "B"}`;
+    }
+    if (num >= 1_000_000) {
+      const formatted = (num / 1_000_000).toFixed(2);
+      return `${formatted.length > 7 ? (num / 1_000_000).toFixed(1) + "M" : formatted + "M"}`;
+    }
+    return num.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+  };
+  
+  export const formatNumberWithTwoDecimals = (num: number): string => {
+    return num.toLocaleString(undefined, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 10,
+    });
+  };
+  
+  export const formatPercentage = (num: number): string => {
+    return `${num.toFixed(2)}%`;
+  };
+  
